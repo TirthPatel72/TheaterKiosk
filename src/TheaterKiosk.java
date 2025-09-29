a5db442c8cc800f405a0f5c4a9cf2592ee63bee9
TirthPatel72 <104174533+TirthPatel72@users.noreply.github.com>
2025-09-29 13:32:13 -0400

    Kiosk

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TheaterKiosk {
+    public static void main(String[] args) {
+        int age = 0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        age = in.nextInt();
+
+        if (age >= 21) {
+            System.out.println("You get a wrist band.");
+
+
+        }
+    }
 }
